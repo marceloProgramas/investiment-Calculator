ff7e287b27e3ea218fb4250547a30e8be0a3032a
marceloProgramas <95505900+marceloProgramas@users.noreply.github.com>
2023-07-24 14:23:08 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,4 +7,3 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 )
-//https://www.figma.com/file/Lz1Y7e9XTBIXUud8MLs7oH/eCommerce---Flower-Delivery-website-%7C-UI-%26-UX-%7C-Ui-kit-%7C-Template-(Community)?type=design&node-id=1331-6392&mode=design&t=wdDSbtGNN8R0OyVB-0
